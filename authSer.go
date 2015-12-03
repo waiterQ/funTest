17bf81e7562dbe519b91ad146c24e4889a3c6e1a
少秋 <516595344@516595344@qq.com>
2015-12-03 09:18:15 +0800

    test

---
+++ authSer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"auth"
+	"fmt"
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"os"
+)
+
+const (
+	NetworkAddr = "127.0.0.1:9005"
+)
+
+type AuthSer struct{}
+
+func (*AuthSer) Register(name string, pwd string) (res string, err error) {
+	res = name + "+" + pwd
+	fmt.Println(res)
+	return
+}
+
+func main() {
+	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+
+	serverTransport, err := thrift.NewTServerSocket(NetworkAddr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	handler := &AuthSer{}
+	processor := auth.NewAuthSerProcessor(handler)
+
+	server := thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
+	fmt.Println("authThrift server in :" + NetworkAddr)
+	server.Serve()
+}
---
+++ authSer.thrift
@@ -0,0 +1,6 @@
+namespace go auth
+namespace js auth
+
+service authSer {
+	string Register(1:string name, 2:string pwd)
+}
